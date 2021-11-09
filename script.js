87b7bbe134ca641d991cc423f53d00937e9bd8c8
Tian Xu <xueqingxu0302@gmail.com>
2021-11-08 22:39:28 -0700

    done

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("click", myFunction);
+document.addEventListener("click", someOtherFunction);
+
+function myFunction() {
+  alert ("Welcome!")
+}
+
+function someOtherFunction() {
+  alert ("Bye!")
+}
